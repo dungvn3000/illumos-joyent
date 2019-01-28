a8a41db39d55501d8905d72c0ec47d878bc22d6c
Rob Johnston <rob.johnston@joyent.com>
2019-01-28 23:44:29 +0000

    Remove #pragma from usr/src/lib/libefi/common/crc32_efi.c Breaks build of format_image

--- usr/src/lib/libefi/common/crc32_efi.c
+++ usr/src/lib/libefi/common/crc32_efi.c
@@ -23,9 +23,6 @@
  * Copyright 2002 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
-
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 #include <sys/crc32.h>
 
 static unsigned int crc32_tab[] = { CRC32_TABLE };
